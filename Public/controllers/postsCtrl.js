0eed7071c572b0346f5497db2f2358c31e2328e3
beretkag <beretkag@gmail.com>
2022-12-19 09:33:17 +0100

    postCtrl posts and comments

--- Public/controllers/postsCtrl.js
+++ Public/controllers/postsCtrl.js
@@ -11,6 +11,15 @@ app.controller('postsCtrl', function($scope, $rootScope, DB) {
         $scope.emoticons = res.data;
     });
 
+    DB.selectAll('postdetails').then(function(res) {
+        $scope.posts=res.data;
+        $scope.posts.forEach(post => {
+            DB.select('commentdetails', 'postID', post.ID).then(function(res){
+                post.comments = res.data;
+            });
+        }); 
+    });
+
     DB.selectAll('emotions').then(function(res) {
         $scope.posts = res.data;
         $scope.posts.forEach(item => {
@@ -19,4 +28,5 @@ app.controller('postsCtrl', function($scope, $rootScope, DB) {
             });
         });
     });
+    
 });
