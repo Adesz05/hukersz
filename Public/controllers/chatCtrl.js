c4e3db025119d52f3fad1dc3ae7f5727c557beea
beretkag <beretkag@gmail.com>
2023-01-11 10:22:25 +0100

    chat (kész)

--- Public/controllers/chatCtrl.js
+++ Public/controllers/chatCtrl.js
@@ -2,17 +2,41 @@ app.controller('chatCtrl', function($scope, $rootScope, DB) {
     $scope.messages = [];
 
 
+    getMessages();
 
-    DB.select('messages', 'fromID', $rootScope.loggedUser.ID).then(function(res) {
-        //???????????????????????????
-    });
+    function getMessages() {
+        DB.select('messages', 'fromID', $rootScope.loggedUser.ID).then(function(res) {
+            $scope.messages = res.data;
+    
+            DB.select('messages', 'toID', $rootScope.loggedUser.ID).then(function(resu) {
+                $scope.messages = $scope.messages.concat(resu.data);
+                for (let i = 0; i < $scope.messages.length; i++) {
+                    if (!($scope.messages[i].toID == $rootScope.partner.ID || $scope.messages[i].fromID == $rootScope.partner.ID)) {
+                        $scope.messages.splice(i, 1);
+                    }
+                }
+                $scope.messages.sort((a, b) => {return a.ID - b.ID});
+                $scope.messages.forEach(message => {
+                    message.date = moment(message.date).format('YYYY-MM-DD H:mm:ss');
+                    if (message.fromID == $rootScope.partner.ID) {
+                        message.name = $rootScope.partner.name;
+                    } else {
+                        message.name = $rootScope.loggedUser.name;
+                    }
+                });
+                setTimeout(() => {
+                    document.querySelector('#messageboxok').scrollTop = document.querySelector('#messageboxok').scrollHeight - document.querySelector('#messageboxok').clientHeight;
+                }, 200)
+            });
+        });
+    } 
 
     $scope.sending = function(){
         if ($scope.newMessage.length > 0) {
             let data = {
                 fromID: $rootScope.loggedUser.ID,
                 toID: $rootScope.partner.ID,
-                date: moment(new Date()).format('YYYY-MM-DD H:mm'),
+                date: moment(new Date()).format('YYYY-MM-DD H:mm:ss'),
                 message: $scope.newMessage
             }
             DB.insert('messages', data).then(function(res) {
@@ -20,10 +44,19 @@ app.controller('chatCtrl', function($scope, $rootScope, DB) {
                     alert('Váratlan hiba történt az adatbázis művelet során!');
                 } else {
                     data.ID = res.data.insertId;
+                    data.name = $rootScope.loggedUser.name;
                     $scope.messages.push(data);
                     $scope.newMessage = "";
+                    setTimeout(() => {
+                        document.querySelector('#messageboxok').scrollTop = document.querySelector('#messageboxok').scrollHeight - document.querySelector('#messageboxok').clientHeight;
+                    }, 200)
+                    
                 }
             })
         }
+    }
+
+    $scope.reFresh = function() {
+        getMessages();
     }
 });
